4b4038d496b44ad9935ccced31a4ba4f435032b2
abdulkadirbir <abdulkadirbirr@gmail.com>
2021-01-20 21:38:16 +0000

    modul initial

---
+++ src/Module.php
@@ -0,0 +1,15 @@
+<?php
+namespace abdulkadirbir\content;
+class Module extends \yii\base\Module 
+{
+    public function init()
+    {    
+        
+        parent::init();    
+    
+    }
+
+
+
+}
+
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,9 @@
+<?php
+namespace abdulkadirbir\content\controllers;
+class DefaultController extends \yii\web\Controller
+{
+public function actionIndex()
+{
+return $this->render('index');
+}
+}
