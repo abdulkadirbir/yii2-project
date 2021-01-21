3ef8e42221ae8f844082bfcba7d7047d23358e86
abdulkadirbir <abdulkadirbirr@gmail.com>
2021-01-21 22:33:43 +0300

    updating

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -1,1 +1,43 @@
+
 <?php echo "OKEY"; ?>
+<html>
+<head>
+<title>MAHALLENİN LİSTESİ</title>
+</head>
+<body>
+
+
+
+<form action="" method="post">
+<h1> Mahalleye Yeni İnsan Ekle</h1>
+<table cellpadding="5" cellspacing="5">
+    <tr>
+        <td>İsim :</td>
+        <td><input type="text" name="isim" /></td>
+    </tr>
+
+    <tr>
+        <td>Soy İsim :</td>
+        <td><input type="text" name="soyisim" /></td>
+    </tr>
+
+    <tr>
+        <td>Memleket :</td>
+        <td><input type="text" name="memleket" /></td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td><input type="submit" value="Ekle" /></td>
+    </tr>
+</table>
+</form>
+
+
+
+</body>
+</html>
+
+
+
+
